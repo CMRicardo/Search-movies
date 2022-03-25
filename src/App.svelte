11afb98879397d73e3c5633e7b59a5fd66e4f58c
CMRicardo <ricardocorrales84@gmail.com>
2022-03-24 19:13:22 -0600

    Initialize project with Vite

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<Counter initialCounter={2} />
+<Counter initialCounter={10} />
+<Counter />
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let initialCounter = 0;
+  let counter = initialCounter;
+
+  function handleClick() {
+    counter++;
+  }
+</script>
+
+<main>
+  <h1>Number: {counter}</h1>
+  <button on:click={handleClick}>Increment</button>
+</main>
+
+<style>
+  main {
+    text-align: center;
+  }
+  h1 {
+    color: blueviolet;
+  }
+  button {
+    border: none;
+    padding: 0.5rem 1rem;
+    border-radius: 5px;
+    background: blueviolet;
+    color: white;
+  }
+</style>
